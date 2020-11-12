5243ec6d96ef25387f0e9d92077b356ea7e8fab5
Kishana Liburd <kishana.liburd@gmail.com>
2020-11-12 15:09:55 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ string_values = " "
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    if src[row_index][element_index].class. == String 
+    if src[row_index][element_index].class == String 
     string_values += src[row_index][element_index] + " "
   #binding.pry
   end
